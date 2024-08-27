fc367d431777fbbe91c7e669b5b5a450df1729a1
Jonathan Sellier <jonosellier@gmail.com>
2024-08-27 16:25:15 -0400

    Add ToS/PP

--- src/components/notifications.svelte
+++ src/components/notifications.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { notificationStore } from '$lib/notification.service';
 	function close(idx: number) {
 		notificationStore.update((n) => n.filter((_, i) => i !== idx));
 	}
+
+	let privOpen = true;
 </script>
 
 <div class="fixed bottom-0 end-0 z-50 w-80 me-2">
+	{#if privOpen}
+		<div
+			class="px-4 py-3 mb-2 flex justify-between items-center border rounded-lg bg-slate-700 text-slate-100 border-slate-500"
+		>
+			<div>
+				By using this website, you are agreeing to our terms of use and privacy policy which is
+				located <a class="text-orange-500" href={`${base}/privacy`}>here</a>
+
+				<button class="btn-sm btn-default block mt-4" on:click={() => (privOpen = false)}
+					>I understand</button
+				>
+			</div>
+		</div>
+	{/if}
+
 	{#each $notificationStore as n, index}
 		<div
 			class={'px-4 py-3 mb-2 flex justify-between items-center border rounded-lg ' +
